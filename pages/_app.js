3f4a4596fc781c417efec9e907d19daff738d67f
YOGESH <yogesh000aryal@gmail.com>
2021-11-24 22:36:36 +0545

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import React, {useEffect,useState} from 'react'
+import {Layout} from '../components/index'
 import 'tailwindcss/tailwind.css'
+import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      {/* commonly known as children of the layout and redirects to index.js*/}
+      <Component {...pageProps} /> 
+    </Layout>
+  )
 }
 
 export default MyApp
