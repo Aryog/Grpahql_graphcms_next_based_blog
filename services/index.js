acc9bcc628d8d5850ddfef988eecc4fc8d978422
YOGESH <yogesh000aryal@gmail.com>
2021-11-25 13:30:33 +0545

    "refactor commit"

--- services/index.js
+++ services/index.js
@@ -84,7 +84,7 @@ export const getRecentPosts =async()=>
     query GetPostDetails(){
         posts(
             orderBy : createdAt_DESC
-            last : 3
+            first : 3
         )
             {
              title
@@ -106,6 +106,7 @@ export const getSimilarPosts = async (categories,slug)=>{
     query GetPostDetails($slug:String!, $categories : [String!]){
         posts(
             where:{slug_not: $slug, AND:{categories_some:{slug_in:$categories}} }
+            orderBy : createdAt_DESC
             last:3
         )
         {
